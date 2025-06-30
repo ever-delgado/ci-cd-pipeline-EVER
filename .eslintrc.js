c401002b308df41e5c7c01778fe74a73ed60ec31
Daniel Delgado <eddelgadocanaza@gmail.com>
2025-06-30 12:32:44 -0400

    chore: update package.json to include devDependencies and dependencies, improve linting scripts, and format keywords

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,30 @@
 module.exports = {
   env: {
     node: true,
+    commonjs: true,
     es2021: true,
-    jest: true
+    jest: true,
   },
   extends: [
-    'eslint:recommended'
+    'airbnb-base',
+    'plugin:jest/recommended',
+    'eslint:recommended',
   ],
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
+    ecmaVersion: 'latest',
   },
   rules: {
+    // Tus reglas personalizadas pueden sobreescribir las de airbnb-base
     'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
     'quotes': ['error', 'single'],
     'semi': ['error', 'always'],
-    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-    'no-console': 'off'
-  }
+    'no-console': 'off', // Se mantiene para permitir logs en el servidor
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_|next' }], // Advierte sobre variables no usadas, ignorando _ y next
+
+    // Reglas específicas para Jest
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+  },
 };
